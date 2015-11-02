8b429f1447947d933232e3a2b281b05deada493b
Jong Eun Lee <yomybaby@yomybaby.com>
2015-11-03 06:46:44 +0900

    request의 args를 promise resolve로 전달 및 사용

--- device.js
+++ device.js
@@ -37,6 +37,8 @@ httpReq({
     ni: optionData.node_ID						//등록한 장치의 식별자 (ni == nodeID) 
   }
 }).then(function(result){
+  console.log(colors.blue('1. node 생성 요청 내용'));
+  console.log(result.requestArgs);
   console.log(colors.green('1. node 생성 결과'));
   if(result.statusCode == 409){
     console.log('이미 생성된 node resource ID 입니다.');
