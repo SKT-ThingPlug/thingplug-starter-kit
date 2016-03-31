6d8e47d1580bd9144b78292ca38b90c92fb5ae82
CT Lab 이호상 <michael.hosang.lee@sk.com>
2016-03-31 09:30:21 +0900

    add dashboard support APIs and refactoring

--- application.js
+++ application.js
@@ -2,109 +2,33 @@
 Application에서 data에 접근하기 위해서는 uKey만을 사용합니다.
 실행에 앞서 thingplug 사이트에서  사용자와 계정에 디바이스 등록을 진행해야합니다.
 */
-
 'use strict';
-
 var colors = require('colors');
-var httpReq = require('./promise-http').request;
+var async = require('async');
 
-var optionData = require('./config');
+var config = require('./config');
+var api = require('./lib/api');
 
-// Request ID를 생성하기 위한 RandomInt Function
-function randomInt (low, high) {
-	return Math.floor(Math.random() * (high - low + 1) + low);
-}
-
-// 1. ContentInstance를 활용한 서버에 저장된 센서 값 조회(Retrieve)
-httpReq({
-  options: {
-    host : 'sandbox.sktiot.com',
-    port: '9000',
-    path : '/ThingPlug/remoteCSE-'+ optionData.node_ID+ '/container-'+optionData.container_name+'/latest',
-    method: 'GET',
-    headers : {
-      Accept: 'application/json',
-      locale: 'ko',
-      uKey : optionData.uKey,
-      'X-M2M-RI': randomInt(100000, 999999),
-      'X-M2M-Origin': optionData.app_ID
-    }
+async.waterfall([
+  function (cb){
+    api.getLatestContainer(config.nodeID, config.containerName, cb);
+  },
+  function(data, cb){
+    console.log('content : ' + data.con);
+    console.log('resouceId : ' + data.ri);
+    console.log('생성일 : '+ data.ct);
+    var cmd = JSON.stringify({'cmd':'open'});
+    api.reqMgmtCmd(config.nodeID, config.mgmtCmdPrefix, cmd, cb);
   }
-}).then(function(result){
-  console.log(colors.green('1. latest contentInstance 조회'));
-  if(result.data){
-		var data = JSON.parse(result.data);
-		console.log('content : ' + data.con);
-		console.log('resouceId : ' + data.ri);
-		console.log('생성일 : '+ data.ct);
+], function(err,resourceID){
+  if(err){
+    return console.log(err);
   }
-
-  return httpReq({ // 2. mgmtCmd 요청
-    options: {
-      host : 'sandbox.sktiot.com',
-      port: '9000',
-      path : '/ThingPlug/mgmtCmd-' + optionData.mgmtCmd_prefix + optionData.node_ID,
-      method: 'PUT',
-      headers : {
-        Accept: 'application/json',
-        uKey : optionData.uKey,
-        'X-M2M-Origin': optionData.app_ID,
-        'X-M2M-RI': randomInt(100000, 999999),
-				'Content-Type': 'application/json',
-      }
-    },
-		body : {
-			exra : 'testArgument',			//제어 요청(일반적으로 원격 장치를 RPC호출)을 위한 Argument 정의 (exra == execReqArgs)
-			exe : true						//제어 요청 Trigger 속성으로 해당 속성은 (True/False로 표현) (exe == execEnabler)
-											//해당 속성을 True로 지정하고 PUT 요청을 ThingPlug에 전송하면 제어 동작을 진행함
-		}
-  });
-}).then(function(result){
-  console.log(colors.green('2. mgmtCmd 제어 요청'));
-	//console.log(result);
-  if(result.data){
-		var data = JSON.parse(result.data);
-		console.log('resourceID : '+data.exin[0].ri);
-		console.log('execStatus : '+data.exin[0].exs);
-
-		checkMgmtResults(data.exin[0].ri);
-  }
-
-}).catch(function(err){
-  // console.log(colors.red('2. mgmtCmd 제어 요청 에러'));
-  console.log(err);
-
+  setInterval( function(){
+    api.getMgmtResults(config.nodeID, config.mgmtCmdPrefix, resourceID, function(err,data){
+      if(err) return console.log(err);
+      console.log('resourceId : ' + data.ri);
+      console.log('execStatus : ' + data.exs);
+    });
+  },1000);
 });
-
-function checkMgmtResults(resourceID){
-  return httpReq({ //  execInstance 리소스 조회
-     options: {
-       host : 'sandbox.sktiot.com',
-       port: '9000',
-       path : '/ThingPlug/mgmtCmd-'+ optionData.mgmtCmd_prefix + optionData.node_ID +'/execInstance-'+ resourceID,
-       method: 'GET',
-       headers : {
-         Accept: 'application/json',
-         locale: 'ko',
-         uKey : optionData.uKey,
-         'X-M2M-RI': '12345',
-         'X-M2M-Origin': 'ThingPlug'
-       }
-     }
-   }).then(function(result){
-     console.log(colors.green('#. execInstance 리소스 조회'));
-     if(result.data){
-			 var data = JSON.parse(result.data);
-			 console.log('resourceID : ' + data.ri);
-			 console.log('execStatus : ' + data.exs);
-
-			 if(data.exs === 2){
-		 		 setTimeout(function(){
-					 checkMgmtResults(resourceID);
-				 },1000);
-			 }
-     }
-   }).catch(function(err){
-     console.log(err);
-  });
-}
--- device.js
+++ device.js
@@ -1,246 +1,86 @@
 'use strict';
 
 var colors = require('colors');
-var parseString = require('xml2js').parseString;
-var httpReq = require('./promise-http').request;
+var xml2js = require('xml2js');
+var async = require('async');
 
-var optionData = require('./config');
+var config = require('./config');
+var api = require('./lib/api');
+var MQTTClient = require('./lib/mqtt_client');
+
+var UPDATE_CONTENT_INTERVAL = 1000;
+var BASE_TEMP = 30;
 
 console.log(colors.green('### ThingPlug Device###'));
-if(typeof optionData == 'undefined') {
-  console.log(colors.red('먼저 config.js를 열어 optionData를 설정하세요. README.md에 Starterkit 실행 방법이 설명되어 있습니다.'));
-  return;
+if(typeof config === 'undefined') {
+  return console.log(colors.red('먼저 config.js를 열어 optionData를 설정하세요. README.md에 Starterkit 실행 방법이 설명되어 있습니다.'));
 }
 
-// Request ID를 생성하기 위한 RandomInt Function
-function randomInt (low, high) {
-  return Math.floor(Math.random() * (high - low + 1) + low);
-}
-
-// 1. node 생성
-httpReq({
-  options: {
-    host : 'sandbox.sktiot.com',
-    port: '9000',
-    path : '/ThingPlug',
-    method: 'POST',
-    headers : {
-      'X-M2M-Origin': optionData.node_ID,				//해당 요청 메시지 송신자의 식별자
-      'X-M2M-RI': randomInt(100000, 999999),		//해당 요청 메시지에 대한 고유 식별자 (RI == Request ID) / 해당 식별자는 CSE가 자동 생성
-      'X-M2M-NM': optionData.node_ID,           //해당 요청으로 생성하게 되는 자원의 이름 (NM == Name)
-      'Accept': 'application/json',
-      'Content-Type': 'application/json;ty=14', //ty는 생성하고자 하는 Resource Type의 식별자 (ty == 14은 node를 의미함)
-    }
+async.waterfall([
+  function createNode(cb){
+    console.log(colors.blue('1. node 생성 요청'));
+    api.createNode(config.nodeID, cb);
   },
-  body : {
-    ni: optionData.node_ID						//등록한 장치의 식별자 (ni == nodeID)
+  function createRemoteCSE(nodeRI, cb){
+    console.log(colors.blue('2. remoceCSE 생성 요청'));
+    config.nodeRI = nodeRI;
+    api.createRemoteCSE(config.nodeID, config.nodeRI, config.passCode, cb);
+  },
+  function createContainer(dKey, cb){
+    console.log(colors.blue('3. container 생성 요청'));
+    config.dKey = dKey;
+    api.createContainer(config.nodeID, config.containerName, dKey, cb);
+  },
+  function createMgmtCmd(res, cb){
+    console.log(colors.blue('4. mgmtCmd 생성 요청'));
+    var mgmtCmd = config.mgmtCmdPrefix + config.nodeID;
+    api.createMgmtCmd(mgmtCmd, config.dKey, config.cmdType, config.nodeRI, cb);
   }
-}).then(function(result){
-  console.log(colors.blue('1. node 생성 요청 내용'));
-  //console.log(result.requestArgs);
-  console.log(colors.green('1. node 생성 결과'));
-  if(result.statusCode == 409){
-    console.log('이미 생성된 node resource ID 입니다.');
-  }
-  optionData.nodeRI = JSON.parse(result.data).ri;
-  console.log(colors.yellow('생성 node Resource ID : ') + optionData.nodeRI);
-
-  // 2. remoteCSE생성 요청(기기등록)
-  return httpReq({
-    options: {
-      host : 'sandbox.sktiot.com',
-      port: '9000',
-      path : '/ThingPlug',														//rty는 생성하고자 하는 Resource Type의 식별자 (rty == 16은 remoteCSE를 의미함)
-      method: 'POST',
-      headers : {
-        'X-M2M-Origin': optionData.node_ID,										//해당 요청 메시지 송신자의 식별자
-        'X-M2M-RI': randomInt(100000, 999999),									//해당 요청 메시지에 대한 고유 식별자 (RI == Request ID) / 해당 식별자는 CSE가 자동 생성
-        'X-M2M-NM': optionData.node_ID,											//해당 요청으로 생성하게 되는 자원의 이름 (NM == Name)
-        'passCode': optionData.passCode,
-        'Accept': 'application/json',
-        'Content-Type': 'application/json;ty=16'
-      }
-    },
-    body : {
-      cst : 3,      															//등록하는 CSE의 타입 (IN-CSE = 1, MN-CSE = 2, ASN-CSE = 3) (cseType == cst)
-      csi : optionData.node_ID, 												//등록하는 CSE의 식별자 (CSE-ID == csi)
-      poa : ['MQTT|'+optionData.node_ID], 										//등록하는 CSE의 물리적 접근 식별자 또는 주소 (pointOfAccess == poa)
-      rr : true,  																//등록하는 CSE가 접근하는 한 객체 여부 표기 (requestReachability == rr)
-      nl : optionData.nodeRI
+], function processResult (err, result) {
+    if(err){
+      console.log('Registration Failure: ');
+      return console.log(err);
     }
-  });
+    console.log(colors.green('5. content Instance 주기적 생성 시작'));
+    setInterval( function(){
+      var value = Math.floor(Math.random() * 5) + BASE_TEMP;
+      api.createContentInstance(config.nodeID, config.containerName, config.dKey, value);
+    },UPDATE_CONTENT_INTERVAL);
 
-}).then(function(result){
-  console.log(colors.green('2. remoteCSE 생성 결과'));
-  if(result.statusCode == 409){
-    console.log('이미 생성된 remoteCSE 입니다.');
-  }
-  if(result.headers.dkey){
-    console.log('다비이스 키 : '+ result.headers.dkey);
-    console.log('content-location: '+ result.headers['content-location']);		//생성된 자원의 URI
-    optionData['dKey'] = result.headers.dkey;
-  }
-  // MQTT Connect
-  return new Promise(function(resolve, reject){
-    var msgTest;
-    var mqtt = require('mqtt');
-    var client  = mqtt.connect('mqtt://sandbox.sktiot.com');
-
-    client.on('connect', function () {
-      console.log('### mqtt connected ###');
-      client.subscribe("/oneM2M/req/+/"+ optionData.node_ID);
-      resolve();
-    });
-
-    client.on('error', function(error){
-      reject(error);
-    });
-
-    client.on('message', function (topic, message) {
-      // message is Buffer
+    console.log(colors.green('6. 제어 명령 수신 MQTT 연결'));
+    var mqttClient = new MQTTClient(config.nodeID);
+    mqttClient.on('message', function(topic, message){
       var msgs = message.toString().split(',');
       console.log(colors.red('#####################################'));
       console.log(colors.red('MQTT 수신'));
-      parseString( msgs[0], function(err, xmlObj){
+      xml2js.parseString( msgs[0], function(err, xmlObj){
         if(!err){
           console.log(xmlObj['m2m:req']['pc'][0]['exin'][0]['ri'][0]);
-          updateExecInstance(xmlObj['m2m:req']['pc'][0]['exin'][0]['ri'][0]);
+          console.log(xmlObj['m2m:req']['pc'][0]['exin'][0]['exra'][0]);
+          try{
+            var req = JSON.parse(xmlObj['m2m:req']['pc'][0]['exin'][0]['exra'][0]);
+          }
+          catch(e){
+            console.error(xmlObj['m2m:req']['pc'][0]['exin'][0]['exra'][0]);
+            console.error(e);
+          }
+          processCMD(req);
+          var ei = xmlObj['m2m:req']['pc'][0]['exin'][0]['ri'][0];
+          api.updateExecInstance(config.nodeID, config.mgmtCmdPrefix, config.dKey, ei);
         }
       });
       console.log(colors.red('#####################################'));
     });
-  });
-}).then(function(result){
-
-  // 3. container 생성 요청
-  return httpReq({
-    options: {
-      host : 'sandbox.sktiot.com',
-      port: '9000',
-      path : '/ThingPlug/remoteCSE-'+ optionData.node_ID,				//rty == 3은 생성하고자 하는 container 자원을 의미함
-      method: 'POST',
-      headers : {
-        'X-M2M-Origin': optionData.node_ID,										//해당 요청 메시지 송신자의 식별자
-        'X-M2M-RI': randomInt(100000, 999999),									//해당 요청 메시지에 대한 고유 식별자 (RI == Request ID) / 해당 식별자는 CSE가 자동 생성
-        'X-M2M-NM': optionData.container_name,									//해당 요청으로 생성하게 되는 자원의 이름 (NM == Name)
-        'dkey' : optionData['dKey'],
-        'locale': 'ko',
-        'Accept': 'application/json',
-        'Content-Type': 'application/json;ty=3'
-      }
-    },
-    body : {
-      containerType : 'heartbeat',
-      heartbeatPeriod : 300
-    }
-  });
-}).then(function(result){
-  console.log(colors.green('3. container 생성 결과'));
-  if(result.statusCode == 409){
-    console.log('이미 생성된 container 입니다.');
-  }
-  console.log('content-location: '+ result.headers['content-location']);		//생성된 자원의 URI
-
-
-  // 4. 장치 제어를 위한 device mgmtCmd 리소스 생성
-  return httpReq({
-    options: {
-      host : 'sandbox.sktiot.com',
-      port: '9000',
-      path : '/ThingPlug',				//rty == 12는 생성하고자 하는 mgmtCmd 자원을 의미함
-      method: 'POST',
-      headers : {
-        Accept: 'application/json',
-        locale: 'ko',
-        dkey : optionData['dKey'],
-        'X-M2M-Origin': optionData.node_ID,										//해당 요청 메시지 송신자의 식별자
-        'X-M2M-RI': randomInt(100000, 999999),							  //해당 요청 메시지에 대한 고유 식별자 (RI == Request ID) / 해당 식별자는 CSE가 자동 생성
-        'X-M2M-NM': optionData.mgmtCmd_prefix + optionData.node_ID,	//해당 요청으로 생성하게 되는 자원의 이름 (NM == Name)
-        'Content-Type': 'application/json;ty=12'
-      }
-    },
-    body: {
-      cmt : optionData.cmdType,   //장치 제어 형태 (예, Firmware Update, Memory Check) / (cmt == cmdType)
-      exe : true,                 //장치 제어를 위한 Trigger Attribute (true/false) / (exe == execEnable))
-      ext : optionData.nodeRI     //제어되는 장치의 식별자로 제어하고자 하는 장치의 node 자원 식별자를 명시함 (ext == exeTarget)
-    }
-  });
-}).then(function(result){
-  console.log(colors.green('4. mgmtCmd 생성 결과'));
-  if(result.statusCode == 409){
-    console.log('이미 생성된 mgmtCmd 입니다.');
-  }
-  console.log('content-location: '+ result.headers['content-location']);		//생성된 자원의 URI
-  if(result.headers){
-    console.log(colors.green('5. content Instance 주기적 생성 시작'));
-    setContentInterval();
-  }
-}).catch(function(err){
-  console.log(err);
 });
 
-
-function setContentInterval(){
-  setInterval(function(){
-    // 5. content Instance 생성
-    var value = Math.floor(Math.random() * 40);
-    httpReq({
-      options : {
-        host : 'sandbox.sktiot.com',
-        port: '9000',
-        path : '/ThingPlug/remoteCSE-'+ optionData.node_ID+ '/container-'+optionData.container_name,		//rty == 4는 생성하고자 하는 contentInstance 자원을 의미함
-        method: 'POST',
-        headers : {
-          Accept: 'application/json',
-          locale: 'ko',
-          'X-M2M-Origin': optionData.node_ID,
-          'X-M2M-RI': randomInt(100000, 999999),
-          'Content-Type': 'application/json;ty=4',
-          dKey : optionData['dKey']
-        }
-      },
-      body : {
-        cnf : 'text', //업로드 하는 데이터 타입의 정보 (cnf = contentInfo)
-        con : value   //업로드 하는 데이터 (con == content)
-      }
-    }).then(function(result){
-      //console.log(result.requestArgs);
-      //console.log('#####################################');
-      //console.log(result.headers);
-      //console.log(result.data);
-      var data = JSON.parse(result.data);
-      console.log('content : ' + data.con + ', resourceID : '+data.ri);
-    }).catch(function(err){
-      console.log(err);
-    });
-  },1000);
-}
-
-
-function updateExecInstance(ei){
-  httpReq({ // ### execInstance Update(PUT) - execStatus 변경됨
-    options: {
-      host : 'sandbox.sktiot.com',
-      port: '9000',
-      path : '/ThingPlug/mgmtCmd-' + optionData.mgmtCmd_prefix + optionData.node_ID + '/execInstance-'+ei,
-      method: 'PUT',
-      headers : {
-        Accept: 'application/json',
-        dKey : optionData.dKey,
-        'X-M2M-Origin': optionData.node_ID,
-        'X-M2M-RI': randomInt(100000, 999999),
-        'Content-Type': 'application/json',
-        locale: 'ko'
-      }
-    },
-    body : {}
-  }).then(function(result){
-    // console.log(colors.red('#####################################'));
-    var data = JSON.parse(result.data);
-    console.log('처리한 resouceId : ' + data.ri);
-    console.log('처리한 결과 execStatus : ' + data.exs);
-    console.log(colors.red('#####################################'));
-  }).catch(function(err){
-    console.log(err);
-  });
+function processCMD(req){
+  if(req.cmd === 'on'){
+    BASE_TEMP = 10;
+  }
+  else if(req.cmd === 'off'){
+    BASE_TEMP = 30;
+  }
+  else{
+    console.error('Unknown cmd: ' + req);
+  }
 }
