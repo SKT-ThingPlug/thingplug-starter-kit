d7dfe1b471556a56b8855c505edd50e9d24caf57
CT Lab 이호상 <michael.hosang.lee@sk.com>
2016-03-31 12:13:34 +0900

    add missing config for edison

---
+++ example/edison/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'board' : 'edison',
+  'sensor' : [
+    {
+      'name' : 'light_1',
+      'category' : 'light',
+      'pin' : 'A0'
+    },
+    {
+      'name' : 'button_1',
+      'category' : 'button',
+      'pin' : 5
+    },
+    {
+      'name' : 'led_1',
+      'category' : 'led',
+      'pin' : 3
+    }
+  ]
+}
